f043baaa93dca5a65922c40a770e6b41a803fecc
Sushmitha DT <dtsushmitha@gmail.com>
2020-02-25 21:20:05 +0530

    added authentication in Auth middleware

--- middlewares/authMidware.js
+++ middlewares/authMidware.js
@@ -3,5 +3,21 @@ const config = require("config");
 
 module.exports = function(req, res, next) {
   console.log("Auth middleware..");
-  next();
+
+  // 1. Get JWT token from request header
+  // 2. If no JWT in header send 401 response
+  // 3. Verify JWT with secret key send 400 response
+
+  const token = req.header("x-jwt");
+
+  if (!token) res.status(401).send("No Token in request header (x-jwt)");
+
+  try {
+    const tokenDecoded = jwt.verify(token, config.get("JWTSecretKey"));
+    req.user = tokenDecoded;
+    console.log(req.user);
+    next();
+  } catch (ex) {
+    res.status(400).send("Invalid Token");
+  }
 };
