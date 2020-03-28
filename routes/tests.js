7b907fcafec9da6b59584d2f2c9d1db2cd1e76df
Sushmitha DT <41836325+Sushmitha-93@users.noreply.github.com>
2020-03-29 01:07:24 +0530

    Post api for tests

--- routes/tests.js
+++ routes/tests.js
@@ -4,9 +4,23 @@ const { Test } = require("../models/testModel");
 
 router.get("/", async (req, res) => {
   let tests;
-  console.log(Object.keys(req.query)); // Gives property names in the object. Here they are query key names - class, month
+  console.log(Object.keys(req.query)); // Gives property names in the object. Here they are query property names - class, month
   tests = await Test.find(req.query);
   res.send(tests);
+});
+
+router.post("/", async (req, res) => {
+  let test = new Test({
+    name: req.body.name,
+    class: req.body.class,
+    month: req.body.month,
+    subjects: req.body.subjects ? req.body.subjects : []
+  });
+
+  test = await test
+    .save()
+    .then(() => res.send(test))
+    .catch(err => res.status(400).send(err.name + ": " + err.errmsg)); // Catches insertion errors
 });
 
 module.exports = router;
